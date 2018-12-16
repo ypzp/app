06e931957e1b3d3d2f2ea8113eef38b880d44727
ypzp <707684610@qq.com>
2018-12-16 19:51:20 +0800

    配置文件

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require("path");
+
+
+module.exports={
+	entry:"./src/index.js",
+	output:{
+	   filename:"a.js",
+	   path: path.resolve(__dirname,"dist") 
+	}
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
